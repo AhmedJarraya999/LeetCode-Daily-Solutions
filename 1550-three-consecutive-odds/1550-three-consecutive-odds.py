06daed54da4b9098f79d15b0de4c5f6ec261fe86
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-05-11 16:48:12 +0100

    Time: 0 ms (100%), Space: 12.6 MB (46.71%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.py
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def threeConsecutiveOdds(self, arr):
         for i in range(len(arr)):
-            if( (arr[i]%2!=0) and (arr[i-1]%2!=0) and (arr[i-2]%2!=0)  and (i>=2)):
+            if( (i>=2) and  (arr[i]%2!=0) and (arr[i-1]%2!=0) and (arr[i-2]%2!=0) ):
                 return True
         return False
