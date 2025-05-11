445e9c25ee5f69cfc5fe7298e9677beaa71b7c5f
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-05-11 15:37:41 +0100

    Time: 0 ms (100%), Space: 12.3 MB (100%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,22 +1,23 @@
 class Solution(object):
     def scoreOfParentheses(self, s):
-        score=1
         stack=[]
-        output=0
-        for i in range(len(s)):
+        res=0
+        val=0
+        for i in range (len(s)):
             if s[i]=="(":
                 stack.append(0)
-            elif s[i]==")":
+            else:
                 latest=stack.pop()
                 if latest==0:
                     val=1
                 else:
-                    val=latest*2
+                    val=latest *2
                 if not stack:
-                    output+=val
+                    res+=val
                 else:
                     stack[-1]+=val
-        return output
+        return res
+
 
  
 
