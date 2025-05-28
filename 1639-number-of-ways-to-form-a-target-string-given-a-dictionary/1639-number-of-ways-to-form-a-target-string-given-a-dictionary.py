b23d2dc499932427773e7b79c63d25d00ecf4d3a
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-05-28 10:52:45 +0100

    Time: 2685 ms (12.26%), Space: 405 MB (8.54%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,29 +1,30 @@
+MOD = 10**9 + 7
+
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        cnt=defaultdict(int)
+        from collections import defaultdict
+        cnt = defaultdict(int)
+
         for w in words:
-            for i,c in enumerate(w):
-                cnt[(i,c)]+=1
-        dp={}#map i,j to num f ways we can build the target word
-        #i index of target j index of word
-        def dfs(i,j):
-            if i==len(target):
+            for i, c in enumerate(w):
+                cnt[(i, c)] += 1
+
+        dp = {}  # memoization: (i, k) → number of ways to build target[i:] from words[k:]
+
+        def dfs(i, k):
+            if i == len(target):
                 return 1
-            if j==len(words[0]):
+            if k == len(words[0]):
                 return 0
-            if (i,j) in dp:
-                return dp[(i,j)]
-            c=target[i]
-            dp[(i,j)]=dfs(i,j+1)# skip i position
-            dp[(i,j)]+=cnt[(j,c)] * dfs(i+1,j+1)
-            return dp[(i,j)]%(10**9+7)
-        return dfs(0,0)
-            
+            if (i, k) in dp:
+                return dp[(i, k)]
 
+            res = dfs(i, k + 1)  # skip column k
+            c = target[i]
+            res += cnt[(k, c)] * dfs(i + 1, k + 1)
+            res %= MOD
 
-                
+            dp[(i, k)] = res
+            return res
 
-
-        
-
-        
+        return dfs(0, 0)
