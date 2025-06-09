c09dc377d38b971d5e435e0cc0e67a47a5049592
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-06-09 12:11:13 +0100

    Time: 148 ms (81.91%), Space: 18.9 MB (75.2%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)  # Correct search range: speed from 1 to max(piles)
+        res = r
+        
+        def canEat(speed):
+            hours = 0
+            for p in piles:
+                hours += (p + speed - 1) // speed
+            return hours <= h
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if canEat(mid):
+                res = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return res
