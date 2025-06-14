9c26c11ccc7acf678c2ee4e1f339f2238a884d18
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-06-14 13:25:50 +0100

    Time: 0 ms (100%), Space: 17.8 MB (45.89%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.py
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.py
@@ -8,10 +8,16 @@ class Solution:
             if digits[i]!=0:
                 val=digits[i]
                 minlist=[0 if x==val  else x  for x in digits]
+                break
+            else:
+                minlist=digits.copy()
         for i in range(len(digits)):
             if digits[i]!=9:
                 val=digits[i]
                 maxlist=[9 if x==val else x for x in digits]
+                break
+            else:
+                maxlist=digits.copy()
         highest=int("".join(map(str,maxlist)))
         lowest=int("".join(map(str,minlist)))
         return highest-lowest
