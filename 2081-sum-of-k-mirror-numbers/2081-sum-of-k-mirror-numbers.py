db463aca6581c734d434fdf7646c5cfc895b68d9
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-06-23 17:24:13 +0100

    Time: 4194 ms (16.13%), Space: 18 MB (18.71%) - LeetHub

--- 2081-sum-of-k-mirror-numbers/2081-sum-of-k-mirror-numbers.py
+++ 2081-sum-of-k-mirror-numbers/2081-sum-of-k-mirror-numbers.py
@@ -1,31 +1,65 @@
 class Solution:
     def kMirror(self, k: int, n: int) -> int:
-        def base_k(i: int, k: int):
-            # res=""
-            # while i>0:
-            #     res=str(i%k)+res
-            #     i//=k
-            # return res
-            if i == 0:
+        def to_base_k(x: int) -> str:
+            if x == 0:
                 return "0"
             digits = []
-            while i > 0:
-                 digits.append(str(i % k))  
-                 i //= k
-            return ''.join(reversed(digits))  
-        def is_palindrome(n: int) -> bool:
-            s = str(n)
+            while x > 0:
+                digits.append(str(x % k))
+                x //= k
+            return ''.join(reversed(digits))
+
+        def is_palindrome(s: str) -> bool:
             return s == s[::-1]
 
-        cnt=0
-        i=1
-        output=0
-        while cnt<n:
-            if is_palindrome(base_k(i,k)) and is_palindrome(i):
-                cnt+=1
-                output+=i
-            i+=1
-        return output
+        def generate_palindromes():
+            # Start with 1-digit palindromes
+            length = 1
+            while True:
+                # Odd-length palindromes
+                for half in range(10 ** (length - 1), 10 ** length):
+                    s = str(half)
+                    yield int(s + s[-2::-1])  # Ex: 123 -> 12321
+
+                # Even-length palindromes
+                for half in range(10 ** (length - 1), 10 ** length):
+                    s = str(half)
+                    yield int(s + s[::-1])  # Ex: 123 -> 123321
+
+                length += 1
+
+        count = 0
+        total = 0
+        for num in generate_palindromes():
+            if is_palindrome(to_base_k(num)):
+                total += num
+                count += 1
+                if count == n:
+                    break
+
+        return total
+
+# class Solution:
+#     def kMirror(self, k: int, n: int) -> int:
+#         def base_k(i: int, k: int):
+#             res=""
+#             while i>0:
+#                 res=str(i%k)+res
+#                 i//=k
+#             return res
+#         def is_palindrome(n: int) -> bool:
+#             s = str(n)
+#             return s == s[::-1]
+
+#         cnt=0
+#         i=1
+#         output=0
+#         while cnt<n:
+#             if is_palindrome(base_k(i,k)) and is_palindrome(i):
+#                 cnt+=1
+#                 output+=i
+#             i+=1
+#         return output
 
 
         
