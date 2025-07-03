61bb0af034d3f0a1dcdee10c86fda1140b320de8
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-07-03 03:48:31 +0100

    Time: 11 ms (58.39%), Space: 17.7 MB (85.53%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.py
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.py
@@ -1,6 +1,16 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        ones = bin(k - 1).count('1')
-        return chr(ord('a') + ones)
+        # ones = bin(k - 1).count('1')
+        # return chr(ord('a') + ones)
+        res=['a']
+        while len(res)<k:
+            curr=res[:] #copy
+            for i in range(len(curr)):
+                if curr[i]=="z":
+                    curr[i]="a"
+                else:
+                    curr[i]=chr(ord(curr[i])+1)
+            res+=curr
+        return res[k-1]
 
         
