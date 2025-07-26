0be151a5e193e139b42569005eb84511061a73c9
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-07-26 22:36:12 +0100

    Time: 327 ms (99.15%), Space: 85.2 MB (99.15%) - LeetHub

---
+++ 3480-maximize-subarrays-after-removing-one-conflicting-pair/3480-maximize-subarrays-after-removing-one-conflicting-pair.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxSubarrays(self, n: int, conflicting_pairs: List[List[int]]) -> int:
+        m = len(conflicting_pairs)
+        # Ensure each pair is ordered (start <= end)
+        for p in conflicting_pairs:
+            if p[0] > p[1]:
+                p[0], p[1] = p[1], p[0]
+        # Sort by end-point
+        conflicting_pairs.sort(key=lambda p: p[1])
+
+        blocked = 0
+        profit = 0
+        max_profit = 0
+        max1 = 0
+        max2 = 0
+
+        for i, (start, end) in enumerate(conflicting_pairs):
+            bottom = conflicting_pairs[i+1][1] if i < m - 1 else n + 1
+            gap = bottom - end
+
+            if start > max1:
+                max2 = max1
+                max1 = start
+                profit = 0
+            elif start > max2:
+                max2 = start
+
+            profit += (max1 - max2) * gap
+            if profit > max_profit:
+                max_profit = profit
+
+            blocked += max1 * gap
+
+        total_subarrays = n * (n + 1) // 2
+        return total_subarrays - blocked + max_profit
