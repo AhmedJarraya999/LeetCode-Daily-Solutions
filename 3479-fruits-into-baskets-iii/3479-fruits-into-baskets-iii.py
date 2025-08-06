16e935bfe2470b69fbe884c6e33919980e3639ae
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-08-06 23:58:15 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        used_b=set()
+        res=0
+        for i in range(len(fruits)):
+            used_f=0
+            for j in range(len(baskets)):
+                if j in used_b or fruits[i]>baskets[j]:
+                    continue
+                used_b.add(j)
+                used_f=1
+                break
+            if  not used_f:res+=1
+        return res
+        
