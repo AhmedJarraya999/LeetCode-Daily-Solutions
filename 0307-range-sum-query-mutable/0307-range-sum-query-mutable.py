ba845ad712d887b579b0a6ffd2e8942dd969a48d
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-08-07 05:47:14 +0100

    Time: 886 ms (42.61%), Space: 37 MB (44.93%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,11 +1,10 @@
 class NumArray:
-
     def __init__(self, nums: List[int]):
-        self.n=len(nums)
-        self.tree=[0]*(4*self.n)
-        self.nums=nums
+        self.n = len(nums)
+        self.tree = [0] * (4 * self.n)
+        self.nums = nums
         self.build(0, 0, self.n - 1)
-        
+
     def build(self, node: int, l: int, r: int):
         if l == r:
             self.tree[node] = self.nums[l]
@@ -14,10 +13,10 @@ class NumArray:
             self.build(2 * node + 1, l, mid)
             self.build(2 * node + 2, mid + 1, r)
             self.tree[node] = self.tree[2 * node + 1] + self.tree[2 * node + 2]
-        
 
     def update(self, index: int, val: int) -> None:
         self._update(0, 0, self.n - 1, index, val)
+
     def _update(self, node: int, l: int, r: int, index: int, val: int):
         if l == r:
             self.tree[node] = val
@@ -28,10 +27,10 @@ class NumArray:
             else:
                 self._update(2 * node + 2, mid + 1, r, index, val)
             self.tree[node] = self.tree[2 * node + 1] + self.tree[2 * node + 2]
-        
 
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.nums[left:right+1])
+        return self._sumRange(0, 0, self.n - 1, left, right)
+
     def _sumRange(self, node: int, l: int, r: int, left: int, right: int) -> int:
         if right < l or r < left:
             return 0
@@ -40,9 +39,3 @@ class NumArray:
         mid = (l + r) // 2
         return self._sumRange(2 * node + 1, l, mid, left, right) + \
                self._sumRange(2 * node + 2, mid + 1, r, left, right)
-
-
-# Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# obj.update(index,val)
-# param_2 = obj.sumRange(left,right)
