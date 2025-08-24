b32a3fc1b5efc329928591eeda90633d878c3230
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-08-24 02:58:02 +0100

    Time: 77 ms (45.91%), Space: 20.3 MB (27.38%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -4,12 +4,13 @@ class Solution:
         left,right=0,0
         bestsofar=0
         for right in range(len(nums)):
-            if nums[right]==0:
+            if nums[right]==0: #this second one
                 cnt+=1
-            while cnt>k:
+            while cnt>k: #this will be step 3 and it will  be bestsoar if appropriate
                 if nums[left]==0:
                     cnt-=1
                 left+=1
+            #by default this will be executed
             bestsofar=max(bestsofar,right-left+1)
         return bestsofar
         
