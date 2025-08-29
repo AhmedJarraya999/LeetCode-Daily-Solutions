5fe0e20ef75bd44a41dee695c757558b7a94e5c6
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-08-29 13:50:27 +0100

    Time: 4 ms (7.64%), Space: 17.7 MB (61.11%) - LeetHub

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
@@ -1,9 +1,13 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        count=0
-        for x in range(1,n+1):
-            for y in range(1,m+1):
-                if (x+y)%2==1:
-                    count+=1
-        return count
+        oddX,evenX=(n+1)//2,n//2
+        oddY, evenY = (m + 1) // 2, m // 2
+        return oddX * evenY + evenX * oddY
+        #O(n*m)
+        # count=0
+        # for x in range(1,n+1):
+        #     for y in range(1,m+1):
+        #         if (x+y)%2==1:
+        #             count+=1
+        # return count
         
