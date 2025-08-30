4765d630499dcd3e3df803ee66f3d1f374f1016f
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-08-30 02:52:17 +0100

    Time: 3 ms (81.81%), Space: 18.1 MB (5.46%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,40 +1,40 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        n=len(board)
-        rowset=set()
-        colset=set()
-        boxes=[set() for _ in range(9)]
-        for row in range(n):
-            for col in range(n):
-                if board[row][col]=='.':
-                    continue
-                box_index=(col//3)+(row//3)*3
-                if board[row][col] in boxes[box_index]:
-                    return False
-                else:
-                    boxes[box_index].add(board[row][col])
-
-
-
-        for row in range(n):
+            n=len(board)
             rowset=set()
-            for col in range(n):
-                if board[row][col]=='.':
-                    continue
-                if board[row][col] in rowset:
-                    return False
-                rowset.add(board[row][col])
-        for col in range(n):
+            colset=set()
+            boxes=[set() for _ in range(9)]
             for row in range(n):
-                if board[row][col]=='.':
-                    continue
-                if board[row][col] in colset:
-                    return False    
-                colset.add(board[row][col])
-                colset.clear()
-        return True
+                for col in range(n):
+                    if board[row][col]=='.':
+                        continue
+                    box_index=(col//3)+(row//3)*3
+                    if board[row][col] in boxes[box_index]:
+                        return False
+                    else:
+                        boxes[box_index].add(board[row][col])
+
+
+
+            for row in range(n):
+                rowset=set()
+                for col in range(n):
+                    if board[row][col]=='.':
+                        continue
+                    if board[row][col] in rowset:
+                        return False
+                    rowset.add(board[row][col])
+            for col in range(n):
+                colset=set()
+                for row in range(n):
+                    if board[row][col]=='.':
+                        continue
+                    if board[row][col] in colset:
+                        return False    
+                    colset.add(board[row][col]) 
+            return True
 
 
 
 
-        
+            
