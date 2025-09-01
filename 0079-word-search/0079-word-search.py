59322e47031557f9f6d2a347544612d005cd593d
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-09-01 10:33:45 +0100

    Time: 4490 ms (41.08%), Space: 17.8 MB (65.33%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,24 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        cols=len(board[0])
-        rows=len(board)
-        path=set()
-        def dfs(r,c,i):
-            if i==len(word):
-                return True
-            if (r<0 or c<0 or r>=rows or c>=cols or word[i]!=board[r][c] or (r,c) in path):
-                return False
-            path.add((r,c))
-            res=dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1)
-            path.remove((r,c))
-            return res
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r,c,0):
+        def exist(self, board: List[List[str]], word: str) -> bool:
+            rows=len(board)
+            cols=len(board[0])
+            def backtrack(i,r,c,visited):
+                if i==len(word):
                     return True
-        return False 
+                if (r < 0 or r >= rows or c < 0 or c >= cols 
+                    or board[r][c] != word[i] 
+                    or (r, c) in visited):
+                    return False
+                visited.add((r,c))
 
-        
+                res = (backtrack(i+1, r+1, c, visited) or
+                    backtrack(i+1, r-1, c, visited) or
+                    backtrack(i+1, r, c+1, visited) or
+                    backtrack(i+1, r, c-1, visited))
+                visited.remove((r, c))
+                return res
+            for r in range(rows):
+                for c in range(cols):
+                    if backtrack(0, r, c, set()):
+                        return True
+            return False
