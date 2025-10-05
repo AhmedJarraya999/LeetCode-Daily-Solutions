25975712f620e1e6b7d155b5051f08a2895a03dc
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-10-05 19:35:00 +0100

    Time: 27 ms (83.26%), Space: 19.5 MB (44.71%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,24 +1,26 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific,atlantic=set(),set()
-        rows,cols=len(heights),len(heights[0])
-        def dfs(r,c,visit,prevHeight):
-            if (r,c) in visit or r<0 or c<0 or c==cols or r==rows or heights[r][c]<prevHeight:
+        rows=len(heights)
+        cols=len(heights[0])
+        atlantic=set()
+        pacific=set()
+        visited=set()
+        def dfs(r,c,visited,prevHeight):
+            if (r<0 or c<0 or r==rows or c==cols or(r,c) in visited or heights[r][c]<prevHeight):
                 return
-            visit.add((r,c))
-            dfs(r+1,c,visit,heights[r][c])
-            dfs(r-1,c,visit,heights[r][c])
-            dfs(r,c+1,visit,heights[r][c])
-            dfs(r,c-1,visit,heights[r][c])
+            visited.add((r,c))
+            directions=[[-1,0],[1,0],[0,-1],[0,1]]
+            for dr,dc in directions:
+                dfs(r+dr,c+dc,visited,heights[r][c])
+        #dfs for all pacific cells
         for c in range(cols):
             dfs(0,c,pacific,heights[0][c])
-            dfs(rows-1,c,atlantic,heights[rows-1][c])
+            dfs(rows - 1, c, atlantic, heights[rows - 1][c])
         for r in range(rows):
-            dfs(r,0,pacific,heights[r][0])
-            dfs(r,cols-1,atlantic,heights[r][cols-1])
-        res=[]
-        for r in range(rows):
-            for c in range(cols):
-                if (r,c) in pacific and (r,c) in atlantic:
-                    res.append([r,c])
-        return res
+            dfs(r, 0, pacific, heights[r][0])      # left column
+            dfs(r, cols - 1, atlantic, heights[r][cols - 1])  
+        result = list(pacific & atlantic)
+        return result
+
+    
+        
