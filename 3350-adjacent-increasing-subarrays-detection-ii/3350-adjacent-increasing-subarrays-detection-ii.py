a545ea37c6ac60bfb0e26e6d6992d177200a0bb3
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-10-16 15:09:47 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.py
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.py
@@ -1,22 +1,47 @@
 class Solution:
     def maxIncreasingSubarrays(self, nums: List[int]) -> int:
-        n = len(nums)
-        
-        # Step 1: increasing lengths ending at each i
-        left = [1] * n
-        for i in range(1, n):
-            if nums[i] > nums[i - 1]:
-                left[i] = left[i - 1] + 1
-        
-        # Step 2: increasing lengths starting at each i
-        right = [1] * n
-        for i in range(n - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
-                right[i] = right[i + 1] + 1
-        
-        # Step 3: find max possible k
-        ans = 0
-        for i in range(n - 1):
-            ans = max(ans, min(left[i], right[i + 1]))
-        
+        n=len(nums)
+        left=[1]*n
+        right=[1]*n
+        for i in range(1,n):
+            if nums[i]>nums[i-1]:
+                left[i]=1+left[i-1]
+        for i in range(n-2,-1,-1):
+            if nums[i+1]>nums[i]:
+                right[i]=right[i+1]+1
+        ans=0
+        for i in range(n-2):
+            ans=max(ans,min(left[i],right[i+1]))
         return ans
+
+
+
+
+
+
+
+
+
+        # n = len(nums)
+
+        # # Step 1: increasing lengths ending at each i
+        # left = [1] * n
+
+        # for i in range(1, n):
+        #     if nums[i] > nums[i - 1]:
+        #         left[i] = left[i - 1] + 1
+                
+
+        # # Step 2: increasing lengths starting at each i
+        # right = [1] * n
+        # for i in range(n - 2, -1, -1):
+        #     if nums[i] < nums[i + 1]:
+        #         right[i] = right[i + 1] + 1
+
+
+        # # Step 3: find max possible k
+        # ans = 0
+        # for i in range(n - 1):
+        #     ans = max(ans, min(left[i], right[i + 1]))
+
+        # return ans
