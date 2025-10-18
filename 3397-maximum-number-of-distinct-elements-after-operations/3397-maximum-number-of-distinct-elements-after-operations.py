a44718f101dd01339ce5a5babc6c3e3511fbdb98
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-10-18 15:15:26 +0100

    Time: 686 ms (90.53%), Space: 31.5 MB (76.92%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.py
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.py
@@ -1,13 +1,32 @@
 class Solution:
     def maxDistinctElements(self, nums: List[int], k: int) -> int:
+        # nums.sort()
+        # used=set()
+        # for num in nums:
+        #     for val in range (num-k,num+k+1):
+        #         if val not in used:
+        #             used.add(val)
+        #             break
+        # return len(used)
         nums.sort()
-        used=set()
+        next_available = float('-inf')
+        count = 0
+        
         for num in nums:
-            for val in range (num-k,num+k+1):
-                if val not in used:
-                    used.add(val)
-                    break
-        return len(used)
+            # The smallest value we can assign to this number
+            low = num - k
+            high = num + k
+            
+            # Ensure we start at least from the next available
+            if next_available < low:
+                next_available = low
+                
+            # If we can assign a value in the range
+            if next_available <= high:
+                count += 1
+                next_available += 1  # move to the next unused number
+                
+        return count
 
 
         
