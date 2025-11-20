9dc1c0064cdc99bd229557a09b1f856e8b87dfaf
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-11-20 17:01:54 +0100

    Time: 0 ms (100%), Space: 17.8 MB (56.9%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -2,12 +2,11 @@ class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         target = tickets[k]
         time = 0
-        
+
         for i, t in enumerate(tickets):
             if i <= k:
                 time += min(t, target)
             else:
                 time += min(t, target - 1)
-        
+
         return time
-        
