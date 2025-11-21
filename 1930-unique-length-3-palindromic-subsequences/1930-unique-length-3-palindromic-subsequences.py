e7ca868932a4b896819be379cea2bb07d7709f9d
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-11-21 20:32:00 +0100

    Time: 1486 ms (24.8%), Space: 18.5 MB (26.6%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,15 +1,33 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        result = 0
-        n = len(s)
+        res=set()
+        left=set()
+        right=Counter(s)
+        for mid in s:
+            right[mid]-=1
+            for c in left:
+                if right[c]>0:
+                    res.add((mid,c))
+            left.add(mid)
+        return len(res)
+
+
+
+
+
+
+
+
+        # result = 0
+        # n = len(s)
     
-        for c in set(s):  # unique characters in s
-            first = s.find(c)
-            last = s.rfind(c)
+        # for c in set(s):  # unique characters in s
+        #     first = s.find(c)
+        #     last = s.rfind(c)
             
-            if first < last:
-                # characters between first and last occurrences
-                middle_chars = set(s[first+1:last])
-                result += len(middle_chars)
+        #     if first < last:
+        #         # characters between first and last occurrences
+        #         middle_chars = set(s[first+1:last])
+        #         result += len(middle_chars)
         
-        return result
+        # return result
