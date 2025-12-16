d3085b3aef4148a063149eb5a001a7e5289d1039
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-12-16 16:28:22 +0100

    Time: 269 ms (51.67%), Space: 22.5 MB (32.22%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,50 +1,46 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
         MOD=10**9+7
-        cache=[[-1]*3 for i in range(len(corridor))]#(i,sats)->count
-        def dfs(i,seats):
-            if i==len(corridor):
-                if seats==2:
-                    return 1
-                else:
-                    return 0
-            if cache[i][seats]!=-1:
-                return cache[i][seats]
-            res=0
-            if seats==2:
-                if corridor[i]=="S":
-                    res=dfs(i+1,1) #we set seats to 1 an not 0 since current is 1
-                else:
-                    res=(dfs(i+1,0)+dfs(i+1,2))%MOD
-            else:
-                if corridor[i]=="S":
-                    res=dfs(i+1,seats+1)
-                else:
-                    res=dfs(i+1,seats)
-            cache[i][seats]=res
-            return res
-        return dfs(0,0)
-        # nb=0
-        # for c in corridor:
-        #     if c=="S":
-        #         nb+=1
-        # if nb<2:
-        #     return 0%MOD
-        # if nb<4:
-        #     return 1%MOD
-        # seat_count=0
-        # segments=[]
-        # start_index =0
-        # for i,c in enumerate(corridor):
-        #     if c=="S":
-        #         seat_count+=1
-        #         if seat_count==1:
-        #             start_index=i
-        #         elif seat_count==2:
-        #             segments.append(corridor[start_index:i+1])
-        #             seat_count=0
+        #combinatroies solution
+        seats=[]
+        for i,c in enumerate(corridor):
+            if c=="S":
+                seats.append(i)
+        l=len(seats)
+        if l<2 or l%2==1:
+            return 0
+        res=1
+        i=1
+        while i <l-1: 
+            res=(res*(seats[i+1]-seats[i]))%MOD
+            i+=2
+        return res
 
 
-        
+
+        # cache=[[-1]*3 for i in range(len(corridor))]#(i,sats)->count
+        # def dfs(i,seats):
+        #     if i==len(corridor):
+        #         if seats==2:
+        #             return 1
+        #         else:
+        #             return 0
+        #     if cache[i][seats]!=-1:
+        #         return cache[i][seats]
+        #     res=0
+        #     if seats==2:
+        #         if corridor[i]=="S":
+        #             res=dfs(i+1,1) #we set seats to 1 an not 0 since current is 1
+        #         else:
+        #             res=(dfs(i+1,0)+dfs(i+1,2))%MOD
+        #     else:
+        #         if corridor[i]=="S":
+        #             res=dfs(i+1,seats+1)
+        #         else:
+        #             res=dfs(i+1,seats)
+        #     cache[i][seats]=res
+        #     return res
+        # return dfs(0,0)
+    
 
         
