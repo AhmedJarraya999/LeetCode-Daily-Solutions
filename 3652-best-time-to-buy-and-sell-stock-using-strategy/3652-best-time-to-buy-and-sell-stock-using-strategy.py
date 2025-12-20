c870d6411793ac809675995f02d8db5773c28100
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2025-12-20 01:31:21 +0100

    Time: 327 ms (28.07%), Space: 33.4 MB (76.57%) - LeetHub

--- 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
@@ -1,37 +1,72 @@
 class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
-        n = len(prices)
-
-        # 1. Original profit
-        base_profit = sum(p * s for p, s in zip(prices, strategy))
-
-        # 2. Prefix sums for prices and strategy * prices
-        prefix_price = [0] * (n + 1)
-        prefix_old = [0] * (n + 1)
-
+        n=len(prices)
+        prefix_price=[0]*(n+1)
+        prefix_old=[0]*(n+1)
         for i in range(n):
             prefix_price[i + 1] = prefix_price[i] + prices[i]
             prefix_old[i + 1] = prefix_old[i] + prices[i] * strategy[i]
+        base_profit=sum(prices[i]*strategy[i] for i in range(n))
+        max_profit=base_profit
+        for start in range(n-k+1): #o(n**2)
+            mid=start+k//2
+            end=start+k
+            old_window=prefix_old[end]-prefix_old[start]
+            new_window=prefix_price[end]-prefix_price[mid]
+            profit=base_profit-old_window+new_window
+            max_profit=max(profit,max_profit)
+        return max_profit 
 
-        max_profit = base_profit
-        half = k // 2
+        #bf
+            # tmp_strategy=strategy.copy()
+            # for i in rang(k/2):
+            #     tmp_strategy[start+i]=0
+            # for i in range(k/2,k):
+            #     temp_strategy[start+i]=1
+            # cur_prof=sum(price[i]*tmp_strategy[i]) for i in range(n)
+            # best=max(best,cur_prof)
+        #bf
+        # return best 
 
-        # 3. Slide window
-        for start in range(n - k + 1):
-            mid = start + half
-            end = start + k
-            # Old contribution of the window
-            old_window = prefix_old[end] - prefix_old[start]
 
-        # New contribution:
-        # first half -> 0
-        # second half -> sell (1)
-            new_window = prefix_price[end] - prefix_price[mid]
+            
 
-        # New total profit
-            new_profit = base_profit - old_window + new_window
-            max_profit = max(max_profit, new_profit)
-        return max_profit
+
+
+
+
+        # n = len(prices)
+
+        # # 1. Original profit
+        # base_profit = sum(p * s for p, s in zip(prices, strategy))
+
+        # # 2. Prefix sums for prices and strategy * prices
+        # prefix_price = [0] * (n + 1)
+        # prefix_old = [0] * (n + 1)
+
+        # for i in range(n):
+        #     prefix_price[i + 1] = prefix_price[i] + prices[i]
+        #     prefix_old[i + 1] = prefix_old[i] + prices[i] * strategy[i]
+
+        # max_profit = base_profit
+        # half = k // 2
+
+        # # 3. Slide window
+        # for start in range(n - k + 1):
+        #     mid = start + half
+        #     end = start + k
+        #     # Old contribution of the window
+        #     old_window = prefix_old[end] - prefix_old[start]
+
+        # # New contribution:
+        # # first half -> 0
+        # # second half -> sell (1)
+        #     new_window = prefix_price[end] - prefix_price[mid]
+
+        # # New total profit
+        #     new_profit = base_profit - old_window + new_window
+        #     max_profit = max(max_profit, new_profit)
+        # return max_profit
 
         #####BF#########
         # n=len(prices)
