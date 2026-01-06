320e4c9a4d3e77c12efc5626d274b4ec7e027f1b
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-01-06 15:27:47 +0100

    Time: 6 ms (36.03%), Space: 20 MB (19.03%) - LeetHub

---
+++ 2923-find-champion-i/2923-find-champion-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findChampion(self, grid: List[List[int]]) -> int:
+        n=len(grid)
+        for i in range(len(grid)):
+            defeated=False
+            for j in range(len(grid)):
+                if i!=j and grid[i][j]==0:
+                    defeated=True
+            if not defeated:
+                return i 
+
+        
