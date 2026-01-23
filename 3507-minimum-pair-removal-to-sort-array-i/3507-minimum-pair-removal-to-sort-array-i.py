7db05eba47b893dc71a2d68c6b86ebd1a6fd783b
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-01-23 13:14:52 +0100

    Time: 23 ms (8.6%), Space: 19.4 MB (19.25%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
@@ -2,28 +2,55 @@ from typing import List
 
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        ops = 0
-        
-        while sorted(nums) != nums:
-            besti = 1
-            
-            # find adjacent pair with minimum sum
+        ops=0
+        while sorted(nums)!=nums:
+            besti=1
             for i in range(2, len(nums)):
-                if nums[i] + nums[i - 1] < nums[besti] + nums[besti - 1]:
+                if nums[i] + nums[i-1] < nums[besti] + nums[besti-1]:
                     besti = i
-            
-            new_nums = []
-            
-            i = 0
-            while i < len(nums):
-                if i == besti - 1:
-                    new_nums.append(nums[i] + nums[i + 1])
-                    i += 2
+            new_nums=[]
+            i=0
+            while i<len(nums):
+                if i==besti-1:
+                    new_nums.append(nums[i]+nums[i+1])
+                    i+=2
                 else:
                     new_nums.append(nums[i])
                     i += 1
-            
-            nums = new_nums
-            ops += 1
-        
+            nums=new_nums
+            ops+=1
         return ops
+
+
+
+
+
+
+
+
+
+        # ops = 0
+        
+        # while sorted(nums) != nums:
+        #     besti = 1
+            
+        #     # find adjacent pair with minimum sum
+        #     for i in range(2, len(nums)):
+        #         if nums[i] + nums[i - 1] < nums[besti] + nums[besti - 1]:
+        #             besti = i
+            
+        #     new_nums = []
+            
+        #     i = 0
+        #     while i < len(nums):
+        #         if i == besti - 1:
+        #             new_nums.append(nums[i] + nums[i + 1])
+        #             i += 2
+        #         else:
+        #             new_nums.append(nums[i])
+        #             i += 1
+            
+        #     nums = new_nums
+        #     ops += 1
+        
+        # return ops
