4bcdd893ba53e9508f41a68f7d0fcc10b5e4f0d5
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-01-23 13:47:21 +0100

    Time: 23 ms (8.6%), Space: 19.3 MB (24.86%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
@@ -1,56 +1,47 @@
-from typing import List
-
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        ops=0
-        while sorted(nums)!=nums:
-            besti=1
-            for i in range(2, len(nums)):
-                if nums[i] + nums[i-1] < nums[besti] + nums[besti-1]:
-                    besti = i
+        op=0
+        while nums!=sorted(nums):
+            idx=1
+            for i in range(2,len(nums)):
+                if nums[i]+nums[i-1]<nums[idx]+nums[idx-1]:
+                    idx=i
             new_nums=[]
             i=0
             while i<len(nums):
-                if i==besti-1:
+                if i==idx-1:
                     new_nums.append(nums[i]+nums[i+1])
                     i+=2
                 else:
                     new_nums.append(nums[i])
-                    i += 1
+                    i+=1
             nums=new_nums
-            ops+=1
-        return ops
+            op+=1
+        return op
 
-
-
-
-
-
-
-
-
-        # ops = 0
         
-        # while sorted(nums) != nums:
-        #     besti = 1
-            
-        #     # find adjacent pair with minimum sum
-        #     for i in range(2, len(nums)):
-        #         if nums[i] + nums[i - 1] < nums[besti] + nums[besti - 1]:
-        #             besti = i
-            
-        #     new_nums = []
-            
-        #     i = 0
-        #     while i < len(nums):
-        #         if i == besti - 1:
-        #             new_nums.append(nums[i] + nums[i + 1])
-        #             i += 2
-        #         else:
-        #             new_nums.append(nums[i])
-        #             i += 1
-            
-        #     nums = new_nums
-        #     ops += 1
         
-        # return ops
+            
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
