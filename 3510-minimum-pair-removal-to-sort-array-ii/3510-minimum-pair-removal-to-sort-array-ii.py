f74e339e5a92e3713e308e591f8cb4e3d8882e68
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-01-23 20:10:21 +0100

    Time: 2080 ms (91.59%), Space: 60.1 MB (44.86%) - LeetHub

--- 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.py
+++ 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.py
@@ -1,48 +1,62 @@
-import heapq
+from heapq import heappush, heappop
 
 class Solution:
     def minimumPairRemoval(self, nums: list[int]) -> int:
-        ops=0
-        n=len(nums)
-        left=[-1]*n
-        right=[-1]*n
-        alive=[True]*n
-        for i in range(n):
-            if i>0:
-                left[i]=i-1
-            if i<n-1:
-                right[i]=i+1
-        heap=[]
-        for i in range(len(nums)-1):
-            heapq.heappush(heap,(nums[i]+nums[i+1],i,i+1))
-        op=0
-        def is_sorted():
-            prev = None
-            for i in range(n):
-                if alive[i]:
-                    if prev is not None and prev > nums[i]:
-                        return False
-                    prev = nums[i]
-            return True
-        while not is_sorted():
-            while heap:
-                s,i,j=heapq.heappop(heap)
-                if not alive[i] or not alive[j] or right[i]!=j:
-                    continue
-                nums[i]+=nums[j]
-                alive[j]=False
-                L=left[i]
-                R=right[j]
-                right[i] = R
-                if R != -1:
-                    left[R] = i
-                
-                # Push new adjacent sums
-                if L != -1:
-                    heapq.heappush(heap, (nums[L] + nums[i], L, i))
-                if R != -1:
-                    heapq.heappush(heap, (nums[i] + nums[R], i, R))
-                ops+=1
-                break
-        return ops
-        
+        n = len(nums)
+        val = [num for num in nums]
+
+        left = [i-1 for i in range(n)]
+        right = [i+1 for i in range(n)]
+
+        # Min-heap of [sum, index]
+        pq = []
+        unsorted = 0
+
+        for i in range(n-1):
+            heappush(pq, (val[i]+val[i+1], i))
+            if val[i] > val[i+1]:
+                unsorted += 1
+
+        ans = 0
+
+        while unsorted > 0 and pq:
+            curr_sum, i = heappop(pq)
+            j = right[i]
+
+            # lazy removal
+            if j >= n or left[j] != i or val[i] + val[j] != curr_sum:
+                continue
+
+            # valid pair
+            if val[i] > val[j]:
+                unsorted -= 1
+
+            prev = left[i]
+            next_ = right[j]
+
+            if prev != -1 and val[prev] > val[i]:
+                unsorted -= 1
+
+            if next_ != n and val[j] > val[next_]:
+                unsorted -= 1
+
+            # merging
+            val[i] = curr_sum
+            right[i] = next_
+            if next_ != n:
+                left[next_] = i
+
+            ans += 1
+
+            # update heap with new adjacent pairs
+            if prev != -1:
+                if val[prev] > val[i]:
+                    unsorted += 1
+                heappush(pq, (val[prev] + val[i], prev))
+
+            if next_ != n:
+                if val[i] > val[next_]:
+                    unsorted += 1
+                heappush(pq, (val[i] + val[next_], i))
+
+        return ans
