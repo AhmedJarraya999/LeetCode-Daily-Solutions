2ceec8970093bc79438de8cfc6de82ad375e5e8d
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-02-02 21:00:56 +0100

    Time: 675 ms (29.62%), Space: 71.5 MB (31.54%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]]) -> int:
+        adj=defaultdict(list)
+        for a,b,w in edges:
+            adj[a].append((b,w))
+            adj[b].append((a,w*2))
+        dist=[0]+[inf]*(n-1)
+        heap=[]
+        heapq.heappush(heap,(0,0))
+        while heap:
+            w,node=heapq.heappop(heap)
+            for nei,nw in adj[node]:
+                if w+nw<dist[nei]:
+                    dist[nei]=w+nw
+                    heapq.heappush(heap,(w+nw,nei))
+        return dist[n-1] if dist[n-1]!=inf else -1
+        # adj={}
+        # shortest={}
+        # for src,dst,weight in adj:
+        #     adj[src].append(dst,weight)
+        # minHeap=[[0,src]]
+        # while minHeap:
+        #     w1,n1=heapq.heappop(minHeap)
+        #     if n1 not in shortest:
+        #         continue
+        #     shortest[n1]=w1
+        #     for n2,w2 in adj[n1]:
+        #         heapq.heappush(minHeap,(W1,+w2,n2))
+        # if n-1 in shortest:
+        #     return shortest[n-1] 
+        # else:
+        #     return -1
+
+        
