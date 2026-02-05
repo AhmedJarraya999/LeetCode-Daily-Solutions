7429e297496c42920e66b5913560faab7f16425c
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-02-05 18:54:03 +0100

    Time: 922 ms (11.27%), Space: 76 MB (16.44%) - LeetHub

--- 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.py
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.py
@@ -1,19 +1,95 @@
 class Solution:
     def minCost(self, n: int, edges: List[List[int]]) -> int:
+        dist=[0]+[inf]*(n-1)
         adj=defaultdict(list)
         for a,b,w in edges:
             adj[a].append((b,w))
             adj[b].append((a,w*2))
-        dist=[0]+[inf]*(n-1)
-        heap=[]
-        heapq.heappush(heap,(0,0))
+        visited=set()
+        heap=[(0,0)]
         while heap:
             w,node=heapq.heappop(heap)
+            if node in visited:
+                continue
+            visited.add(node)
             for nei,nw in adj[node]:
-                if w+nw<dist[nei]:
-                    dist[nei]=w+nw
+                if nei not in visited:
                     heapq.heappush(heap,(w+nw,nei))
-        return dist[n-1] if dist[n-1]!=inf else -1
+                # if w > dist[node]:
+                #     continue   # ← this replaces visited
+                if w+nw<dist[nei]:
+                    dist[nei] = w + nw
+        return dist[n - 1] if dist[n - 1] != inf else -1
+
+
+        ###understand in context of dense graph why it is better to use  a set checker
+        ###understand
+
+        
+
+
+
+
+
+
+
+
+
+        # adj=defaultdict(list)
+        # for a,b,w in edges:
+        #     adj[a].append((b,w))
+        #     adj[b].append((a,w*2))
+        # heap=[(0,0)]
+        # dist = [0] + [inf] * (n - 1)
+        # while heap:
+        #     w,node=heapq.heappop(heap)
+        #     for nei,nw in adj[node]:
+        #         if w+nw<dist[nei]:
+        #             dist[nei] = w + nw
+        #         heapq.heappush(heap,(w+nw,nei))
+        # return dist[n - 1] if dist[n - 1] != inf else -1
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # adj = defaultdict(list)
+        # for a, b, w in edges:
+        #     adj[a].append((b, w))
+        #     adj[b].append((a, w * 2))
+        # dist = [0] + [inf] * (n - 1)
+        # heap = []
+        # heapq.heappush(heap, (0, 0))
+        # while heap:
+        #     w, node = heapq.heappop(heap)
+        #     for nei, nw in adj[node]:
+        #         if w + nw < dist[nei]:
+        #             dist[nei] = w + nw
+        #             heapq.heappush(heap, (w + nw, nei))
+        # return dist[n - 1] if dist[n - 1] != inf else -1
         # adj={}
         # shortest={}
         # for src,dst,weight in adj:
@@ -27,8 +103,6 @@ class Solution:
         #     for n2,w2 in adj[n1]:
         #         heapq.heappush(minHeap,(W1,+w2,n2))
         # if n-1 in shortest:
-        #     return shortest[n-1] 
+        #     return shortest[n-1]
         # else:
         #     return -1
-
-        
