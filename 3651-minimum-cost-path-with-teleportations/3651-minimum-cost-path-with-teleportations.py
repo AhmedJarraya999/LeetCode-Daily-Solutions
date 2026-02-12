dbc55b826840285cc2e009a0e95b0a0f7bc5c961
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-02-12 17:29:06 +0100

    Time: 3762 ms (59.12%), Space: 20.6 MB (95.69%) - LeetHub

--- 3651-minimum-cost-path-with-teleportations/3651-minimum-cost-path-with-teleportations.py
+++ 3651-minimum-cost-path-with-teleportations/3651-minimum-cost-path-with-teleportations.py
@@ -1,44 +1,25 @@
-from math import inf
-from typing import List
-
 class Solution:
     def minCost(self, grid: List[List[int]], k: int) -> int:
-        n, m = len(grid), len(grid[0])
-
-        cost = [[inf] * m for _ in range(n)]
-        cost[n - 1][m - 1] = 0
-
-        max_val = max(max(row) for row in grid)
-        tcost = [inf] * (max_val + 1)
-
-        for t in range(k + 1):
-            # normal DP
-            for i in range(n - 1, -1, -1):
-                for j in range(m - 1, -1, -1):
-                    if i + 1 < n:
-                        cost[i][j] = min(
-                            cost[i][j],
-                            grid[i + 1][j] + cost[i + 1][j]
-                        )
-                    if j + 1 < m:
-                        cost[i][j] = min(
-                            cost[i][j],
-                            grid[i][j + 1] + cost[i][j + 1]
-                        )
-                    if t > 0:
-                        cost[i][j] = min(
-                            cost[i][j],
-                            tcost[grid[i][j]]  # teleport cost = 0
-                        )
-
-            # update teleport helper
+        n=len(grid)
+        m=len(grid[0])
+        cost=[[inf]*m for _ in range(n)]
+        cost[-1][-1]=0  
+        tcost=[inf]*(max(max(row) for row in grid) +1 )
+        for t in range(k+1):
+            for i in range(n-1,-1,-1):
+                for j in range(m-1,-1,-1):
+                    #move right,down or teleport:
+                    if i<n-1:
+                        cost[i][j]=min(cost[i][j],cost[i+1][j]+grid[i+1][j])
+                    if j<m-1:
+                        cost[i][j]=min(cost[i][j],cost[i][j+1]+grid[i][j+1])
+                    if t>0:
+                        cost[i][j]=min(cost[i][j],tcost[grid[i][j]])
+            #compute tcost for the next number of teleportation
             for i in range(n):
                 for j in range(m):
-                    v = grid[i][j]
-                    tcost[v] = min(tcost[v], cost[i][j])
-
-            # prefix min so <= condition is satisfied
-            for v in range(1, len(tcost)):
-                tcost[v] = min(tcost[v], tcost[v - 1])
-
+                    tcost[grid[i][j]]=min(tcost[grid[i][j]],cost[i][j])
+            for i in range(1,len(tcost)):
+                tcost[i]=min(tcost[i],tcost[i-1])#compute prefix minimum
         return cost[0][0]
+       
