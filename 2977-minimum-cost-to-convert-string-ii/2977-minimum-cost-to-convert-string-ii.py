b7781230f0e665212e2e4c593630e857c0b8d23a
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-02-26 02:59:05 +0100

    Time: 743 ms (59.51%), Space: 55.5 MB (8.19%) - LeetHub

--- 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.py
+++ 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.py
@@ -8,9 +8,10 @@ class Solution:
         adj=defaultdict(list)
         for o,c,w in zip(original,changed,cost):
             adj[o].append((c,w))
+        lengths=sorted(set(len(o) for o in original))
         @cache
-        def dijkstra(s):
-            heap=[(0,s)]
+        def dijkstra(source):
+            heap=[(0,source)]
             dst={}
             while heap:
                 cost,node=heapq.heappop(heap)
@@ -18,142 +19,32 @@ class Solution:
                     continue
                 dst[node]=cost
                 for nei,nw in adj[node]:
-                    heapq.heappush(heap,(nw+cost,nei))
-            return dst
-        lengths=sorted(set(len(o) for o in original))
-        @cache
+                    heapq.heappush(heap,(cost+nw,nei))
+            return dst 
+        @cache 
         def dp(i):
             if i==len(source):
-                return 0 #best case
+                return 0
             if source[i]==target[i]:
                 best=dp(i+1)
-            else: best=inf
+            else:   best=inf
             for l in lengths:
-                if l+i>len(source):
+                if i+l>len(source):
                     break
                 s=source[i:i+l]
                 t=target[i:i+l]
-                if s not in adj:
-                    continue
                 dis=dijkstra(s)
                 if t not in dis:
-                    continue
+                    continue 
                 best=min(best,dis[t]+dp(i+l))
-            return best
+            return best 
         res=dp(0)
-        return -1 if res == inf else res
-                
-        # adj=defaultdict(list)
-        # for o,c,w in zip(original,changed,cost):
-        #     adj[o].append((c,w))
-        # lengths = sorted(set(len(o) for o in original))
-        # def dijkstra(source):
-        #     dst={}
-        #     heap=[(0,source)]
-        #     while heap:
-        #         cost,node=heapq.heappop(heap)
-        #         if node in dst:
-        #             continue
-        #         dst[node]=cost
-        #         for nei,nw in adj[node]:
-        #             heapq.heappush(heap,(cost+nw,nei))
-        #     return dst
-        # i=0
-        # cost=0
-        # while i<len(source):
-        #     found=False
-        #     for l in lengths:
-        #         if i+l>len(source):
-        #             continue
-        #         s=source[i:i+l]
-        #         t=target[i:i+l]
-        #         if s==t:
-        #             i+=l
-        #             found=True
-        #             break
-        #         dis=dijkstra(s)
-        #         if t in dis:
-        #             cost+=dis[t]
-        #             i+=l
-        #             found=True
-        #             break
-        #     if not found:return -1
-        # return cost
+        best = -1 if res==inf else res 
+
+
+        return best
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    
-        
-        # adj = defaultdict(list)
-        # for o, c, w in zip(original, changed, cost):
-        #     adj[o].append((c, w))
-        
-        # def dijkstra(start):
-        #     dist = {}
-        #     heap = [(0, start)]
-            
-        #     while heap:
-        #         curr_cost, node = heapq.heappop(heap)
-        #         if node in dist:
-        #             continue
-        #         dist[node] = curr_cost
-                
-        #         for nei, w in adj[node]:
-        #             heapq.heappush(heap, (curr_cost + w, nei))
-            
-        #     return dist
-        
-        # # 🔥 collect all possible transformation lengths
-        # lengths = set(len(o) for o in original)
-        
-        # total_cost = 0
-        # n = len(source)
-        
-        # i = 0
-        # while i < n:
-            
-        #     found = False
-            
-        #     for L in lengths:
-        #         if i + L > n:
-        #             continue
-                
-        #         s = source[i:i+L]
-        #         t = target[i:i+L]
-                
-        #         if s == t:
-        #             i += L
-        #             found = True
-        #             break
-                
-        #         dis = dijkstra(s)
-                
-        #         if t in dis:
-        #             total_cost += dis[t]
-        #             i += L
-        #             found = True
-        #             break
-            
-        #     if not found:
-        #         return -1
-        
-        # return total_cost
-        
