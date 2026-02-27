a8c547b0026bb63782fa952b1ba45fefbd31f3a6
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-02-28 00:35:27 +0100

    Time: 596 ms (87.95%), Space: 26 MB (14.18%) - LeetHub

--- 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.py
+++ 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.py
@@ -2,13 +2,11 @@ from collections import defaultdict
 import heapq
 from typing import List
 from math import inf
-
 class Solution:
     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
         adj=defaultdict(list)
         for o,c,w in zip(original,changed,cost):
             adj[o].append((c,w))
-        lengths=sorted(set(len(o) for o in original))
         @cache
         def dijkstra(source):
             heap=[(0,source)]
@@ -19,32 +17,47 @@ class Solution:
                     continue
                 dst[node]=cost
                 for nei,nw in adj[node]:
-                    heapq.heappush(heap,(cost+nw,nei))
-            return dst 
-        @cache 
+                    heapq.heappush(heap,(nw+cost,nei))
+            return dst
+
+        lengths=set(len(o) for o in original)
+        @cache
         def dp(i):
             if i==len(source):
                 return 0
             if source[i]==target[i]:
-                best=dp(i+1)
-            else:   best=inf
-            for l in lengths:
+                res=dp(i+1)
+            else:
+                res=inf 
+            for l in sorted(lengths):
                 if i+l>len(source):
                     break
                 s=source[i:i+l]
                 t=target[i:i+l]
+                if s not in adj:
+                    continue
                 dis=dijkstra(s)
                 if t not in dis:
-                    continue 
-                best=min(best,dis[t]+dp(i+l))
-            return best 
+                    continue
+                dis=dis[t]
+                res=min(res,dis+dp(i+l))
+            return res
         res=dp(0)
-        best = -1 if res==inf else res 
+        if res==inf:
+            return -1
+        else:
+            return res
+        
 
+        
+        
 
-        return best
-
-
+            
+        
+        
+        
+                
+        
 
 
 
