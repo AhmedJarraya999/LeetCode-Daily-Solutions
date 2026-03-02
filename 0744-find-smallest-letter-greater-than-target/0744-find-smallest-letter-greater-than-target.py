ac39ab88cbeef7f390b97f46fc603e0b55370048
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-03-02 20:59:48 +0100

    Time: 0 ms (100%), Space: 20.7 MB (55.01%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -1,8 +1,22 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        for c in letters:
-            if c>target:
-                return c
-        return letters[0]
+        # for c in letters:
+        #     if c>target:
+        #         return c
+        # return letters[0]
+        left,right=0,len(letters)-1
+        while left<=right:
+            mid=(left+right)//2
+            if letters[mid]<=target:
+                left=mid+1
+            else:
+                right=mid-1
+        if left==len(letters):
+            return letters[0]
+        return letters[left]
 
+        
+
+        
+        
         
