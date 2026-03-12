890a8705896443338c7c979d7bf75e4ef2918574
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-03-12 07:46:12 +0100

    Time: 1591 ms (30.81%), Space: 38.1 MB (20.65%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.py
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -2,19 +2,19 @@ class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
         small=SortedList()
         large=SortedList()
-        n=len(nums)
         res=float('inf')
+        n=len(nums)
         for i in range(1,dist+2):
             small.add((nums[i],i))
         while len(small)>=k:
             s=small.pop(-1)
             large.add(s)
         curr=sum(s for s,_ in small)
-        res=min(res,curr)
+        res=curr
         for i in range(dist+2,n):
-            nkey=(nums[i],i)
             pkey=(nums[i-dist-1],i-dist-1)
-            ###element 5raaj
+            nkey=(nums[i],i)
+            ###process li bech n5ajou element lekdiim
             if pkey in small:
                 small.discard(pkey)
                 curr-=pkey[0]
@@ -24,15 +24,19 @@ class Solution:
                     curr+=l[0]
             elif pkey in large:
                 large.discard(pkey)
-            ###element d5aaal
+            ##process li bech nda5lou fih eleement jdid that was added to window
             small.add(nkey)
             curr+=nkey[0]
             if len(small)>=k:
                 s=small.pop(-1)
-                curr-=s[0]
                 large.add(s)
+                curr-=s[0]
             res=min(res,curr)
         return nums[0]+res
+
+
+
+       
 
 
 
