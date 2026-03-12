3d350f82f1b09ee7088af6a6048730a66d2eee43
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-03-12 04:17:32 +0100

    Time: 1514 ms (49.67%), Space: 38 MB (21.07%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.py
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -1,47 +1,41 @@
-from sortedcontainers import SortedList
-from typing import List
-
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
-        n = len(nums)
-
-        small = SortedList()
-        large = SortedList()
-
-        # initial window
-        for i in range(1, dist + 2):
-            small.add((nums[i], i))
-
-        while len(small) >= k:
+        small=SortedList()
+        large=SortedList()
+        n=len(nums)
+        res=float('inf')
+        ##initially our window
+        for i in range(1,dist+2):
+            small.add((nums[i],i))
+        while len(small)>=k:
             large.add(small.pop(-1))
-
-        curr = sum(v for v, _ in small)
-        res = curr
-
-        for i in range(dist + 2, n):
-            pkey = (nums[i - dist - 1], i - dist - 1)
-            nkey = (nums[i], i)
-
+        curr=sum(s for s,_ in small)
+        res=min(res,curr)
+        ##iterate our window
+        for i in range(dist+2,n):
+            pkey=(nums[i-dist-1],i-dist-1)
+            nkey=(nums[i],i)
             if pkey in small:
                 small.discard(pkey)
-                curr -= pkey[0]
-
+                curr-=pkey[0]
                 if large:
-                    l = large.pop(0)
+                    l=large.pop(0)
                     small.add(l)
-                    curr += l[0]
-
+                    curr+=l[0]
             elif pkey in large:
                 large.discard(pkey)
-
             small.add(nkey)
-            curr += nkey[0]
-
-            if len(small) >= k:
-                s = small.pop(-1)
+            curr+=nkey[0]
+            if len(small)>=k:
+                s=small.pop(-1)
+                curr-=s[0]
                 large.add(s)
-                curr -= s[0]
+            res=min(res,curr)
+        return nums[0]+res
 
-            res = min(res, curr)
 
-        return res + nums[0]
+
+
+
+
+        
