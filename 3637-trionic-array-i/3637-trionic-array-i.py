e8f74876fd299c23797872b6662b8f3ce8831fc3
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-03-16 13:51:19 +0100

    Time: 0 ms (100%), Space: 19.2 MB (99.18%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.py
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -1,30 +1,52 @@
 class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
-        n = len(nums)
-        state = 0   # 0: inc, 1: dec, 2: inc
+        state=0
         if nums[0]>=nums[1]:
              return False
-
-
-        for i in range(2, n):
-            if state == 0:
-                if nums[i] > nums[i-1]:
+        for i in range(2,len(nums)):
+            if state==0:
+                if nums[i]>nums[i-1]:
                     continue
-                elif nums[i] < nums[i-1]:
-                    state = 1
+                elif nums[i]<nums[i-1]:
+                    state=1
                 else:
                     return False
-
-            elif state == 1:
-                if nums[i] < nums[i-1]:
+            if state==1:
+                if nums[i]<nums[i-1]:
                     continue
-                elif nums[i] > nums[i-1]:
-                    state = 2
+                elif nums[i]>nums[i-1]:
+                    state=2
                 else:
                     return False
-
-            else:  # state == 2
-                if nums[i] <= nums[i-1]:
+            else:
+                if nums[i]<=nums[i-1]:
                     return False
+        return state==2
+        # n = len(nums)
+        # state = 0   # 0: inc, 1: dec, 2: inc
+        # if nums[0]>=nums[1]:
+        #      return False
 
-        return state == 2
+
+        # for i in range(2, n):
+        #     if state == 0:
+        #         if nums[i] > nums[i-1]:
+        #             continue
+        #         elif nums[i] < nums[i-1]:
+        #             state = 1
+        #         else:
+        #             return False
+
+        #     elif state == 1:
+        #         if nums[i] < nums[i-1]:
+        #             continue
+        #         elif nums[i] > nums[i-1]:
+        #             state = 2
+        #         else:
+        #             return False
+
+        #     else:  # state == 2
+        #         if nums[i] <= nums[i-1]:
+        #             return False
+
+        # return state == 2
