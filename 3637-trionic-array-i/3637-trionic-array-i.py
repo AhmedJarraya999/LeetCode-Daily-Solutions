06ecb26c1ad30bbd7f97d5298f23e734227510e0
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-03-16 14:26:32 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.py
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -2,7 +2,7 @@ class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
         state=0
         if nums[0]>=nums[1]:
-             return False
+            return False
         for i in range(2,len(nums)):
             if state==0:
                 if nums[i]>nums[i-1]:
@@ -22,31 +22,5 @@ class Solution:
                 if nums[i]<=nums[i-1]:
                     return False
         return state==2
-        # n = len(nums)
-        # state = 0   # 0: inc, 1: dec, 2: inc
-        # if nums[0]>=nums[1]:
-        #      return False
-
-
-        # for i in range(2, n):
-        #     if state == 0:
-        #         if nums[i] > nums[i-1]:
-        #             continue
-        #         elif nums[i] < nums[i-1]:
-        #             state = 1
-        #         else:
-        #             return False
-
-        #     elif state == 1:
-        #         if nums[i] < nums[i-1]:
-        #             continue
-        #         elif nums[i] > nums[i-1]:
-        #             state = 2
-        #         else:
-        #             return False
-
-        #     else:  # state == 2
-        #         if nums[i] <= nums[i-1]:
-        #             return False
-
-        # return state == 2
+            
+        
