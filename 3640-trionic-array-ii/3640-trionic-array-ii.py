e06ab38653074cbf51a79388f5201fe57c7ed407
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-04-02 18:31:58 +0100

    Time: 165 ms (65.81%), Space: 32.8 MB (44.39%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.py
+++ 3640-trionic-array-ii/3640-trionic-array-ii.py
@@ -1,58 +1,40 @@
 class Solution:
-    def maxSumTrionic(self, nums: List[int]) -> int: 
-        i=0
-        res=float('-inf')
-        while i<len(nums):
-            j=i+1
-            ##first segmentsum
-            while j<len(nums) and nums[j]>nums[j-1]:
-                j+=1
-            p=j-1
-            if p==i:
-                i+=1
+    def maxSumTrionic(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = 0
+        ans = -inf
+        while i < n:
+            l = i
+            i += 1
+            while i < n and nums[i - 1] < nums[i]:
+                i += 1
+            if i == l + 1:
                 continue
-            cur=nums[p]+nums[p-1]
-            ## second segment sum
-            while j<len(nums) and nums[j]<nums[j-1]:
-                cur+=nums[j]
-                j+=1
-            q=j-1
-            if p==q or q==len(nums)-1 or nums[q]==nums[j]:
-                i=q
+
+            p = i - 1
+            s = nums[p - 1] + nums[p]
+            while i < n and nums[i - 1] > nums[i]:
+                s += nums[i]
+                i += 1
+            if i == p + 1 or i == n or nums[i - 1] == nums[i]:
                 continue
-            cur+=nums[j]
-            j+=1
-            cum=0
-            mx=0
-            while j<len(nums) and nums[j]>nums[j-1]:
-                cum+=nums[j]
-                mx=max(mx,cum)
-                j+=1
-            cur+=mx
-            jj=p-2
-            acc=0
-            mx=0
-            while jj>=0 and nums[jj]<nums[jj+1]:
-                acc+=nums[jj]
-                mx=max(acc,mx)
-                jj-=1
-            cur+=mx
-            res=max(res,cur)
-            i=q
-        return res
 
+            q = i - 1
+            s += nums[i]
+            i += 1
+            mx = t = 0
+            while i < n and nums[i - 1] < nums[i]:
+                t += nums[i]
+                i += 1
+                mx = max(mx, t)
+            s += mx
 
-            
+            mx = t = 0
+            for j in range(p - 2, l - 1, -1):
+                t += nums[j]
+                mx = max(mx, t)
+            s += mx
 
-        
-                
-
-                
-        
-        
-            
-
-
-        
-            
-            
+            ans = max(ans, s)
+            i = q
+        return ans
