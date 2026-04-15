39c949ca82d40edb550fa2631c5c918d0d0d7bb4
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-04-15 19:54:45 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.py
+++ 3640-trionic-array-ii/3640-trionic-array-ii.py
@@ -14,6 +14,44 @@ class Solution:
             if nums[i]>nums[i-1]:
                 inc2[i]=max(dec[i-1]+nums[i],inc2[i-1]+nums[i])
         return max(inc2)
+### how to reduce this to one DP array with 3 states per index yes
+# from typing import List
+
+# class Solution:
+#     def maxSumTrionic(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         NEG = float('-inf')
+
+#         dp = [[NEG]*3 for _ in range(n)]
+
+#         dp[0][0] = nums[0]   # start increasing
+
+#         for i in range(1, n):
+#             # STATE 0: increasing
+#             if nums[i] > nums[i-1]:
+#                 dp[i][0] = max(
+#                     nums[i],
+#                     dp[i-1][0] + nums[i]
+#                 )
+#             else:
+#                 dp[i][0] = nums[i]
+
+#             # STATE 1: decreasing
+#             if nums[i] < nums[i-1]:
+#                 dp[i][1] = max(
+#                     dp[i-1][0] + nums[i],
+#                     dp[i-1][1] + nums[i]
+#                 )
+
+#             # STATE 2: increasing after decrease
+#             if nums[i] > nums[i-1]:
+#                 dp[i][2] = max(
+#                     dp[i-1][1] + nums[i],
+#                     dp[i-1][2] + nums[i]
+#                 )
+
+#         return max(dp[i][2] for i in range(n))
+
                 
       
         
