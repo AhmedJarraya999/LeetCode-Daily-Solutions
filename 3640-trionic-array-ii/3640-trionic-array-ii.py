df76e1fd6ca7660c7db1a78066357ff324fa4a8f
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-04-15 19:49:02 +0100

    Time: 303 ms (39.15%), Space: 34.7 MB (36.51%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.py
+++ 3640-trionic-array-ii/3640-trionic-array-ii.py
@@ -1,78 +1,16 @@
-# 
-from typing import List
-
 class Solution:
     def maxSumTrionic(self, nums: List[int]) -> int:
-        ###dp[i][state] heere
-        #first did not work 
-        # n = len(nums)
-        # NEG = float('-inf')
-
-        # # dp[i][state]
-        # dp = [[NEG] * 3 for _ in range(n)]
-
-        # # base case: start first element in increasing phase
-        # dp[0][0] = nums[0]
-
-        # for i in range(1, n):
-
-        #     # 1️⃣ Increasing phase (state 0)
-        #     if nums[i] > nums[i - 1]:
-        #         dp[i][0] = max(
-        #             dp[i - 1][0] + nums[i],  # continue increasing
-        #             nums[i - 1] + nums[i]    # start new increasing segment
-        #         )
-
-        #     # 2️⃣ Decreasing phase (state 1)
-        #     if nums[i] < nums[i - 1]:
-        #         dp[i][1] = max(
-        #             dp[i - 1][0] + nums[i],  # switch from increasing → decreasing
-        #             dp[i - 1][1] + nums[i]   # continue decreasing
-        #         )
-
-        #     # 3️⃣ Second increasing phase (state 2)
-        #     if nums[i] > nums[i - 1]:
-        #         dp[i][2] = max(
-        #             dp[i - 1][1] + nums[i],  # switch from decreasing → increasing
-        #             dp[i - 1][2] + nums[i]   # continue second increasing
-        #         )
-
-        # return max(dp[i][2] for i in range(n))
-
-
-
-###second
-
-
-
-        n = len(nums)
-        NEG = -10**18  # equivalent to Long.MIN_VALUE / 4
-
-        inc1 = [NEG] * n
-        dec = [NEG] * n
-        inc2 = [NEG] * n
-
-        for i in range(1, n):
-            # First increasing phase
-            if nums[i] > nums[i - 1]:
-                inc1[i] = max(
-                    inc1[i - 1] + nums[i],
-                    nums[i - 1] + nums[i]
-                )
-
-            # Decreasing phase
-            if nums[i] < nums[i - 1]:
-                dec[i] = max(
-                    inc1[i - 1] + nums[i],
-                    dec[i - 1] + nums[i]
-                )
-
-            # Second increasing phase
-            if nums[i] > nums[i - 1]:
-                inc2[i] = max(
-                    dec[i - 1] + nums[i],
-                    inc2[i - 1] + nums[i]
-                )
-
+        inc1=[float('-inf')]*len(nums)
+        dec=[float('-inf')]*len(nums)
+        inc2=[float('-inf')]*len(nums)
+        for i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                inc1[i]=max(nums[i]+inc1[i-1],nums[i]+nums[i-1])
+            if nums[i]<nums[i-1]:
+                dec[i]=max(inc1[i-1]+nums[i],dec[i-1]+nums[i])
+            if nums[i]>nums[i-1]:
+                inc2[i]=max(dec[i-1]+nums[i],inc2[i-1]+nums[i])
         return max(inc2)
-       
+                
+      
+        
