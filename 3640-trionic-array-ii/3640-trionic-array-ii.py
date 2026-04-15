b19859505dfa7da4644c446e9b7322b81b2ccb6e
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-04-15 19:50:20 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.py
+++ 3640-trionic-array-ii/3640-trionic-array-ii.py
@@ -4,10 +4,13 @@ class Solution:
         dec=[float('-inf')]*len(nums)
         inc2=[float('-inf')]*len(nums)
         for i in range(1,len(nums)):
+            #max subarray sum ending at it foolow up order
             if nums[i]>nums[i-1]:
                 inc1[i]=max(nums[i]+inc1[i-1],nums[i]+nums[i-1])
+            #max subarray sum ending at it foolow up  down order
             if nums[i]<nums[i-1]:
                 dec[i]=max(inc1[i-1]+nums[i],dec[i-1]+nums[i])
+            #max subarray sum ending at it foolow up down up order
             if nums[i]>nums[i-1]:
                 inc2[i]=max(dec[i-1]+nums[i],inc2[i-1]+nums[i])
         return max(inc2)
