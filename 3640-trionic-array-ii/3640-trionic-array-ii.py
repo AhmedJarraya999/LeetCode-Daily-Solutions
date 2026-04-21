839380c087cf6076a1b1f3edc3bd0104d4d9b9de
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-04-21 04:38:27 +0100

    Time: 312 ms (32.72%), Space: 34.7 MB (32.04%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.py
+++ 3640-trionic-array-ii/3640-trionic-array-ii.py
@@ -1,55 +1,17 @@
 class Solution:
     def maxSumTrionic(self, nums: List[int]) -> int:
-        inc1 = [float("-inf")] * len(nums)
-        dec = [float("-inf")] * len(nums)
-        inc2 = [float("-inf")] * len(nums)
-        for i in range(1, len(nums)):
-            # max subarray sum ending at i foolow up order
-            if nums[i] > nums[i - 1]:
-                inc1[i] = max(nums[i] + inc1[i - 1], nums[i] + nums[i - 1])
-            # max subarray sum ending at i foolow up  down order
-            if nums[i] < nums[i - 1]:
-                dec[i] = max(inc1[i - 1] + nums[i], dec[i - 1] + nums[i])
-            # max subarray sum ending at i foolow up down up order
-            if nums[i] > nums[i - 1]:
-                inc2[i] = max(dec[i - 1] + nums[i], inc2[i - 1] + nums[i])
+        ##max subarray ending at i followingg inc shape
+        inc1=[float("-inf")]*len(nums)
+        ##max subarray ending at i following inc then dec shape
+        dec=[float("-inf")]*len(nums)
+        ##max subarray ending at i following in dec then inc again shape 
+        inc2=[float("-inf")]*len(nums)
+        for  i in range(1,len(nums)):
+            if nums[i]>nums[i-1]:
+                inc1[i]=max(inc1[i-1]+nums[i],nums[i]+nums[i-1])
+            if nums[i]<nums[i-1]:
+                dec[i]=max(inc1[i-1]+nums[i],dec[i-1]+nums[i])
+            if nums[i]>nums[i-1]:
+                inc2[i]=max(dec[i-1]+nums[i],inc2[i-1]+nums[i])
         return max(inc2)
-
-
-### how to reduce this to one DP array with 3 states per index yes
-# from typing import List
-
-# class Solution:
-#     def maxSumTrionic(self, nums: List[int]) -> int:
-#         n = len(nums)
-#         NEG = float('-inf')
-
-#         dp = [[NEG]*3 for _ in range(n)]
-
-#         dp[0][0] = nums[0]   # start increasing
-
-#         for i in range(1, n):
-#             # STATE 0: increasing
-#             if nums[i] > nums[i-1]:
-#                 dp[i][0] = max(
-#                     nums[i],
-#                     dp[i-1][0] + nums[i]
-#                 )
-#             else:
-#                 dp[i][0] = nums[i]
-
-#             # STATE 1: decreasing
-#             if nums[i] < nums[i-1]:
-#                 dp[i][1] = max(
-#                     dp[i-1][0] + nums[i],
-#                     dp[i-1][1] + nums[i]
-#                 )
-
-#             # STATE 2: increasing after decrease
-#             if nums[i] > nums[i-1]:
-#                 dp[i][2] = max(
-#                     dp[i-1][1] + nums[i],
-#                     dp[i-1][2] + nums[i]
-#                 )
-
-#         return max(dp[i][2] for i in range(n))
+        
