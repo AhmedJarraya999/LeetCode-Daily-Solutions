c0e8f6567948ab256c15fc3d29a9a59983050a30
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-04-27 14:15:58 +0100

    Time: 120 ms (82.07%), Space: 34.3 MB (99.29%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.py
@@ -0,0 +1,25 @@
+# class Solution:
+#     def minRemoval(self, nums: List[int], k: int) -> int:
+#         if len(nums)==1:
+#             return 0
+#         nums.sort()
+#         i=0
+#         j=1
+#         while nums[j]<=k*nums[i] and j<len(nums)-2:
+#             j+=1
+#         return len(nums)-(j-i+1)
+class Solution:
+    def minRemoval(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        n = len(nums)
+        
+        i = 0
+        max_len = 0
+        
+        for j in range(n):
+            while nums[j] > k * nums[i]:
+                i += 1
+            max_len = max(max_len, j - i + 1)
+        
+        return n - max_len
+        
