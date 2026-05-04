8c6d329e2b7e0d52cd9eb7d009f8758bba43edaa
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-05-04 15:45:52 +0100

    Time: 119 ms (87.03%), Space: 34.7 MB (37.57%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.py
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.py
@@ -25,17 +25,39 @@
 #         return n - max_len
 class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        #idea expand j while nums[j]<=k*nums[i]
         n=len(nums)
+        nums.sort()
         i=0
-        j=0
         max_len=0
-        while j<n:
+        for j in range(n):
             while nums[j]>k*nums[i]:
                 i+=1
             max_len=max(max_len,j-i+1)
-            j+=1
         return n-max_len
+        #
+        # n=len(nums)
+        # nums.sort()
+        # i=0
+        # j=0
+        # while j<n:
+        #     while nums[j]>k*nums[i]:
+        #         i+=1
+        #     max_len=max(max_len,j-i+1)
+        # return n-max_len
+
+            
+        # nums.sort()
+        # n=len(nums)
+        # i=0
+        # j=0
+        # max_len=0
+        # while j<n:
+        #     while nums[j]>k*nums[i]:
+        #         i+=1
+        #     max_len=max(max_len,j-i+1)
+        #     j+=1
+        # return n-max_len
         
     
 
