83fd216a99059ead601a3c6046c853b281d4dccf
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-05-06 13:39:04 +0100

    Time: 907 ms (5.07%), Space: 34.5 MB (70.86%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.py
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.py
@@ -1,14 +1,33 @@
 class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
+        def binarysearch(target):
+            left,right=0,len(nums)
+            while left<right:
+                mid=(left+right)//2
+                if nums[mid]<=target:
+                    left=mid+1
+                else:
+                    right=mid
+            return left
         nums.sort()
         n=len(nums)
-        left=0
-        right=0
         window_size=0
-        for right in range(n):
-            while nums[right]>k*nums[left]:
-                left+=1
-            window_size=max(window_size,right-left+1)
-        return n-window_size
+        for i in range(n):
+            # find first index where value > k * nums[i]
+            # j=bisect_right(nums,k*nums[i])
+            j=binarysearch(k*nums[i])
+            ##window is [i,j-1]
+            window_size=max(window_size,j-i)
+        return n- window_size
+        # nums.sort()
+        # n=len(nums)
+        # left=0
+        # right=0
+        # window_size=0
+        # for right in range(n):
+        #     while nums[right]>k*nums[left]:
+        #         left+=1
+        #     window_size=max(window_size,right-left+1)
+        # return n-window_size
             
         
