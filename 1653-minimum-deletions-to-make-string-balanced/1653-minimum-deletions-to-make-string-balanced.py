115f2d59dd2c16a4e3a031b9ae10ea4fef8bf242
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-05-16 20:54:34 +0100

    Time: 631 ms (11.12%), Space: 27.6 MB (6.13%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,21 +1,26 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        b_count = 0  # number of 'b' seen so far
-        res = 0  # minimum deletions
-        for c in s:
-            if c == "b":
-                b_count += 1
-            else:  # c == 'a'
-                # either delete this 'a', or delete all previous 'b'
-                res = min(res + 1, b_count)
-
-        return res
-        # res=0
-        # b_count=0
+        # b_count = 0  # number of 'b' seen so far
+        # res = 0  # minimum deletions
         # for c in s:
-        #     if c=="b":
-        #         b_count+=1
-        #     else:
-        #         res=min(res+1,b_count)
-        # return res
+        #     if c == "b":
+        #         b_count += 1
+        #     else:  # c == 'a'
+        #         # either delete this 'a', or delete all previous 'b'
+        #         res = min(res + 1, b_count)
 
+        # return res
+        n=len(s)
+        prefix_b=[0]*(n+1)
+        for i in range(len(s)):
+            prefix_b[i+1]=prefix_b[i]+(s[i]=='b')
+        suffix_a = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            suffix_a[i] = suffix_a[i + 1] + (s[i] == 'a')
+        ans = float("inf")
+        for i in range(n + 1):
+            deletions = prefix_b[i] + suffix_a[i]
+            ans = min(ans, deletions)
+        return ans 
+
+ 
