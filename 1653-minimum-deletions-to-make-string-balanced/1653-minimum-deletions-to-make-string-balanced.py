3e4dfe8f8ae0a2b1ae0c10a41bd816b849300171
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-05-16 15:06:56 +0100

    Time: 150 ms (76.72%), Space: 19.9 MB (90.84%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,13 +1,21 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        b_count = 0   # number of 'b' seen so far
-        res = 0       # minimum deletions
-
+        b_count = 0  # number of 'b' seen so far
+        res = 0  # minimum deletions
         for c in s:
-            if c == 'b':
+            if c == "b":
                 b_count += 1
             else:  # c == 'a'
                 # either delete this 'a', or delete all previous 'b'
                 res = min(res + 1, b_count)
 
         return res
+        # res=0
+        # b_count=0
+        # for c in s:
+        #     if c=="b":
+        #         b_count+=1
+        #     else:
+        #         res=min(res+1,b_count)
+        # return res
+
