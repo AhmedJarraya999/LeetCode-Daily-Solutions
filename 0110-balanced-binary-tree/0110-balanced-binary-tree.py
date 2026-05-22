9ee4fa4ee27b58ac4421c9d56a05fff37f742328
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-05-22 16:32:47 +0100

    Time: 2 ms (57.29%), Space: 20.4 MB (40.13%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -7,17 +7,23 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def dfs(node):
-            if not node:
-                return 0
-            left_height=dfs(node.left)
-            if left_height==-1:
-                return -1
-            right_height=dfs(node.right)
-            if right_height == -1:
-                return -1
-            if abs(left_height - right_height) > 1:
-                return -1
-            return 1 + max(left_height, right_height)
-        return dfs(root)!=-1
+            if not node:return  [True,0]
+            left_height,right_height=dfs(node.left),dfs(node.right)
+            balanced=(left_height[0] and right_height[0] and abs(left_height[1]-right_height[1])<=1)
+            return [balanced,1+max(left_height[1],right_height[1])]
+        return dfs(root)[0]
+        # def dfs(node):
+        #     if not node:
+        #         return 0
+        #     left_height=dfs(node.left)
+        #     if left_height==-1:
+        #         return -1
+        #     right_height=dfs(node.right)
+        #     if right_height == -1:
+        #         return -1
+        #     if abs(left_height - right_height) > 1:
+        #         return -1
+        #     return 1 + max(left_height, right_height)
+        # return dfs(root)!=-1
 
         
