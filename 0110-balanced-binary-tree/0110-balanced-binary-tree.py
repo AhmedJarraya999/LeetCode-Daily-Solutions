a6c9767ab8326a47339c7dbbfeacac4363aae3de
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-06-03 12:44:13 +0100

    Time: 0 ms (100%), Space: 20.4 MB (70.96%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,22 +1,15 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        ###at each node the absolute difference btwen hight and right subtree is lower than 1
-        ###the  height is calculated recusrevly if not node return 0 else the height is the max between left and right 
         def height(node):
             if not node:
                 return 0
-
-            left = height(node.left)
-            if left == -1:
+            left_height=height(node.left)
+            right_height=height(node.right)
+            if abs(left_height-right_height)>1:
                 return -1
-
-            right = height(node.right)
-            if right == -1:
+            if left_height==-1:
                 return -1
-
-            if abs(left - right) > 1:
+            if right_height==-1:
                 return -1
-
-            return 1 + max(left, right)
-
-        return height(root) != -1
+            return 1+max(left_height,right_height)
+        return height(root)!=-1
