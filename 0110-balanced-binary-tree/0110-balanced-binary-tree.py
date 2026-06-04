0310eadca48c8d1e94b23af9abf2f71109fb541f
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-06-04 14:19:31 +0100

    Time: 0 ms (100%), Space: 20.4 MB (39.92%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -13,3 +13,18 @@ class Solution:
                 return -1
             return 1+max(left_height,right_height)
         return height(root)!=-1
+
+
+        # def height(node):
+        #     if not node:
+        #         return 0
+        #     left_height=height(node.left)
+        #     right_height=height(node.right)
+        #     if abs(left_height-right_height)>1:
+        #         return -1
+        #     if left_height==-1:
+        #         return -1
+        #     if right_height==-1:
+        #         return -1
+        #     return 1+max(left_height,right_height)
+        # return height(root)!=-1
