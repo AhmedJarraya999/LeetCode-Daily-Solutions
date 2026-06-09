ef56ec67c17c2ba3071bee40d06491572527d5f5
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-06-09 18:27:18 +0100

    Time: 24 ms (86.74%), Space: 26.1 MB (48.54%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -6,27 +6,42 @@
 #         self.right = right
 
 class Solution:
-    def balanceBST(self, root: TreeNode) -> TreeNode:
-        nums = []
-
-        # Step 1: In-order traversal
+    def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        arr=[]
         def inorder(node):
             if not node:
                 return
             inorder(node.left)
-            nums.append(node.val)
+            arr.append(node.val)
             inorder(node.right)
-
         inorder(root)
-
-        # Step 2: Build balanced BST
-        def build(l, r):
-            if l > r:
+        def build(left,right):
+            if left>right:
                 return None
-            mid = (l + r) // 2
-            node = TreeNode(nums[mid])
-            node.left = build(l, mid - 1)
-            node.right = build(mid + 1, r)
+            mid=(left+right)//2
+            node=TreeNode(arr[mid])
+            node.left=build(left,mid-1)
+            node.right=build(mid+1,right)
             return node
-
-        return build(0, len(nums) - 1)
+        return build(0,len(arr)-1)
+        # # Step 1: Inorder traversal to get sorted values
+        # def inorder(node):
+        #     if not node:
+        #         return []
+        #     return inorder(node.left) + [node.val] + inorder(node.right)
+        
+        # # Step 2: Build balanced BST from sorted array
+        # def build(nums, l, r):
+        #     if l > r:
+        #         return None
+            
+        #     mid = (l + r) // 2
+        #     node = TreeNode(nums[mid])
+            
+        #     node.left = build(nums, l, mid - 1)
+        #     node.right = build(nums, mid + 1, r)
+            
+        #     return node
+        
+        # nums = inorder(root)
+        # return build(nums, 0, len(nums) - 1)
