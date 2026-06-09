b440805287c16d3b37d0764bee894098e80cfecc
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-06-09 19:08:08 +0100

    Time: 34 ms (38.1%), Space: 26.6 MB (6.99%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -15,33 +15,14 @@ class Solution:
             arr.append(node.val)
             inorder(node.right)
         inorder(root)
-        def build(left,right):
+        def construct(left,right):
             if left>right:
                 return None
             mid=(left+right)//2
+            # root=arr[mid]
             root=TreeNode(arr[mid])
-            root.left=build(left,mid-1)
-            root.right=build(mid+1,right)
+            root.left=construct(left,mid-1)
+            root.right=construct(mid+1,right)
             return root
-        return build(0,len(arr)-1)
-        # # Step 1: Inorder traversal to get sorted values
-        # def inorder(node):
-        #     if not node:
-        #         return []
-        #     return inorder(node.left) + [node.val] + inorder(node.right)
-        
-        # # Step 2: Build balanced BST from sorted array
-        # def build(nums, l, r):
-        #     if l > r:
-        #         return None
+        return construct(0,len(arr)-1)
             
-        #     mid = (l + r) // 2
-        #     node = TreeNode(nums[mid])
-            
-        #     node.left = build(nums, l, mid - 1)
-        #     node.right = build(nums, mid + 1, r)
-            
-        #     return node
-        
-        # nums = inorder(root)
-        # return build(nums, 0, len(nums) - 1)
