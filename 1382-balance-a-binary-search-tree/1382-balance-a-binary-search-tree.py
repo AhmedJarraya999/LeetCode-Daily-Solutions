9acbb0127dfd9b5f12b825037913389e5e3e0640
AhmedJarraya <96777472+AhmedJarraya999@users.noreply.github.com>
2026-06-11 20:20:13 +0100

    Time: 24 ms (87.35%), Space: 24.5 MB (71.6%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -10,18 +10,22 @@ class Solution:
         arr=[]
         def inorder(node):
             if not node:
-                return 
+                return
             inorder(node.left)
             arr.append(node)
             inorder(node.right)
         inorder(root)
         def construct(left,right):
             if left>right:
-                return None
+                return None 
             mid=(left+right)//2
             root=arr[mid]
             root.left=construct(left,mid-1)
             root.right=construct(mid+1,right)
             return root
         return construct(0,len(arr)-1)
+
+        
+        
+
         
